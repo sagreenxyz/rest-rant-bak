ed0657572f549faa9a64ac26272531e755ab4e41
Scott Green <sagreenxyz@gmail.com>
2022-03-06 20:33:55 -0600

    Setting Up Places  Index Mock Data

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    res.status(200).render('places/index');
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]
+    res.status(200).render('places/index', { places });
 });
 
 module.exports = router;
