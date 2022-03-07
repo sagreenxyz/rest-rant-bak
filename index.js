8c9b99cb5d0c0fc57565057c40567f4eb181b705
Scott Green <sagreenxyz@gmail.com>
2022-03-06 20:29:21 -0600

    Adding 404 page view

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-   res.status(404).send('<h1>404 Page</h1>');
+   res.status(404).render('error404');
 });
 
 app.listen(process.env.PORT, () => {console.log(`Listening on port ${process.env.PORT}: http://localhost:${process.env.PORT} and http://localhost:${process.env.PORT}/places`)});
