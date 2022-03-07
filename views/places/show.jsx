804e1671de85a3ab1438abba885dd916132accd2
Scott Green <sagreenxyz@gmail.com>
2022-03-07 12:21:29 -0600

    Filling Out Show View

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,19 @@
 const React = require('react')
 const Def = require('../default')
 
-function show() {
+function show (data) {
     return (
         <Def>
-            <main>
-                <h1>Show Page</h1>
-            </main>
+          <main>
+            <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt="Image of Place" width="400px" />
+            <h2>Rating</h2>
+            <p>Not Rated</p>
+            <h2>Description</h2>
+            <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines}</p>
+            <h2>Comments</h2>
+            <p>No comments yet!</p>
+          </main>
         </Def>
     )
 }
