423de5095660c486a3565a3fa1af1cc628251164
Scott Green <sagreenxyz@gmail.com>
2022-03-06 20:53:07 -0600

    Adding Image to error404 View

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ function error404() {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/404bird.jpg" alt="404 Bird" width="400px" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@miklevasilyev?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mikhail Vasilyev</a> on <a href="https://unsplash.com/s/photos/free?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
