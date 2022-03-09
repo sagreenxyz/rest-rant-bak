2f6d0e9bb8120e6c4688eba687d212553a39b6b8
Scott Green <sagreenxyz@gmail.com>
2022-03-08 20:42:25 -0600

    Implement the Comment Form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -42,7 +42,7 @@ function show(data) {
                     Serving {data.place.cuisines}
                 </h4>
                 <h2>Comments</h2>
-                {comments}}
+                {comments}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
@@ -50,6 +50,31 @@ function show(data) {
                     <button type="submit" className="btn btn-danger">
                         Delete
                     </button>
+                </form>
+                <hr />
+                <h2>Got Your Own Rant or Rave?</h2>
+                <form action={`/places/${data.place.id}/comment`} method="POST">
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                            <label htmlFor="content">Content</label>
+                            <textarea id="content" name="content" className="form-control"></textarea>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="author">Author</label>
+                            <input id="author" name="author" className="form-control" />
+                        </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="stars">Star Rating</label>
+                            <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                        </div>
+                        <div className="form-group col-sm-2">
+                            <label htmlFor="rant">Rant?</label>
+                            <input type="checkbox" id="rant" name="rant" className="form-control" />
+                        </div>
+                    </div>
+                    <input type="submit" className="btn btn-primary" value="Add Comment" />
                 </form>
             </main>
         </Def>
