c2c2350de916226ccb03b2e399d07c71f1666b39
Scott Green <sagreenxyz@gmail.com>
2022-03-09 03:41:21 -0600

    Create a Comment

--- controllers/places.js
+++ controllers/places.js
@@ -62,12 +62,33 @@ router.get('/:id/edit', (req, res) => {
     res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-    res.send('GET /places/:id/rant stub')
+router.post('/:id/comment', (req, res) => {
+    console.log('post starts here')
+    console.log(req.body)
+    db.Place.findById(req.params.id)
+        .then(place => {
+            db.Comment.create(req.body)
+                .then(comment => {
+                    place.comments.push(comment.id)
+                    place.save()
+                        .then(() => {
+                            res.redirect(`/places/${req.params.id}`)
+                        })
+                })
+                .catch(err => {
+                    console.log('catch1')
+                    res.render('error404')
+                })
+        })
+        .catch(err => {
+            console.log('catch2')
+            res.render('error404')
+        })
+    console.log('post ends here')
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.delete('/:id/comment/:commentId', (req, res) => {
+    res.send('GET /places/:id/comment/:commentId stub')
 })
 
 module.exports = router
