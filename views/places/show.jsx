c3c97a44e932629c4064fd94f8149783458f2130
Scott Green <sagreenxyz@gmail.com>
2022-03-09 03:56:45 -0600

    Calculate Place Ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,21 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div key={c.id} className="border">
@@ -27,7 +41,7 @@ function show(data) {
                 <h1>{data.place.name}</h1>
                 <img src={data.place.pic} alt="Image of Place" width="400px" />
                 <h2>Rating</h2>
-                <p>Not Rated</p>
+                {rating}
                 <h2>Description</h2>
                 <h3>
                     Located in {data.place.city}, {data.place.state}
