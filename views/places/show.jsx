12d76c7e9e57e4b65968800a7cff353a40f9067c
Scott Green <sagreenxyz@gmail.com>
2022-03-08 20:36:08 -0600

    Populate the Comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show(data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div key={c.id} className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <stong>- {c.author}</stong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -23,7 +42,7 @@ function show(data) {
                     Serving {data.place.cuisines}
                 </h4>
                 <h2>Comments</h2>
-                <p>No comments yet!</p>
+                {comments}}
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
                 </a>
